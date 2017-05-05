90e922a22e39428d517ecb7fa4ad4f4b4b92f270
Tim Wu <cvbtassdar@gmail.com>
2017-05-05 08:14:17 +0000

    fix key conditin

--- demos/chat/chatdemo.py
+++ demos/chat/chatdemo.py
@@ -84,7 +84,7 @@ class MessageBuffer(object):
 
     def updateDB(self):
         response =self.db.scan(FilterExpression=(Key('instance').lt(self.instance_id)
-                         | Key('instance').gt(self.instance_id)))
+                         | Key('instance').gt(self.instance_id))
                          & Key('timestamp').gt(int(time.mktime(datetime.datetime.utcnow().timetuple()))), Limit=100)
         if len(response['Items']) > 0:
             messages = []
