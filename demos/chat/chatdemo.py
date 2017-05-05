d12579c7cd3f7e06162b794743e0dbeee1d881c2
Tim Wu <cvbtassdar@gmail.com>
2017-05-05 06:37:13 +0000

    remove timestamp rely

--- demos/chat/chatdemo.py
+++ demos/chat/chatdemo.py
@@ -84,8 +84,8 @@ class MessageBuffer(object):
 
     def updateDB(self):
         response =self.db.scan(FilterExpression=(Key('instance').lt(self.instance_id)
-                         | Key('instance').gt(self.instance_id))
-                         & Key('timestamp').gt(self.global_timestamp), Limit=100)
+                         | Key('instance').gt(self.instance_id)))
+                        # & Key('timestamp').gt(self.global_timestamp-2), Limit=100)
         if len(response['Items']) > 0:
             messages = []
             messages.extend(response['Items'])
